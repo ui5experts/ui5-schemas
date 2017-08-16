6294854bed6ca3131f62284182fb14394b61381a
Christian Schuff <christian.schuff@gmail.com>
2017-08-16 10:48:57 +0200

    adds mininlog improves logging

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,10 @@ const download = require('../lib/download');
 const upgrade = require('../lib/upgrade');
 const link = require('../lib/link');
 const yargs = require('yargs');
+const Minilog = require('minilog');
+
+const log = Minilog('ui5-schemas');
+
 
 const argv = yargs.usage('Usage: ui5-schemas [options]')
   .example('ui5-schemas --sdk openui5 --version 1.28.15', 'Setup with openui5 schemas in version 1.28.15')
@@ -14,6 +18,7 @@ const argv = yargs.usage('Usage: ui5-schemas [options]')
   .describe('outputDir', 'The base directory to output UI5 schemas to.')
   .describe('upgrade', 'Whether to upgrade UI5 schemas for a better development experience or leave them untouched.')
   .describe('link', 'Whether to auto-link UI5 schemas with your favorite IDE (if it is WebStorm ;).')
+  .describe('debug', 'Whether to show debug output')
   .help('h')
   .alias('h', 'help')
   .global(['sdk', 'v', 'outputDir', 'upgrade', 'link', 'debug'])
@@ -28,6 +33,15 @@ const argv = yargs.usage('Usage: ui5-schemas [options]')
   .argv;
 
 
+function createLogger(debug) {
+  const myFilter = new Minilog.Filter();
+  myFilter.clear().deny('ui5-schemas', debug ? '*' : 'debug');
+  Minilog.pipe(myFilter)
+    .pipe(Minilog.backends.console.formatMinilog)
+    .pipe(Minilog.backends.console);
+}
+
+
 exports.ui5_schema = function (options = {
   sdk: 'sapui5',
   version: '',
@@ -35,6 +49,9 @@ exports.ui5_schema = function (options = {
   upgrade: true,
   link: true,
 }) {
+  createLogger(options.debug);
+  log.info(`Preparing '${options.sdk}' schemas in version '${options.version || 'latest'}'...`);
+
   let allLibs = {};
 
   // do the downloading and encapsulate in Promise
@@ -57,7 +74,7 @@ exports.ui5_schema = function (options = {
 
 
   dlPromise.then(() => {
-    console.log('\nYay, all schema files were successfully downloaded!\n');
+    log.info('Yay, all schema files were successfully downloaded!');
     if (options.upgrade) {
       upgrade(options, allLibs);
     }
@@ -65,8 +82,8 @@ exports.ui5_schema = function (options = {
       link(options, allLibs);
     }
   }, (e) => {
-    console.log('Damn... Something went all wrong while downloading the schema files!');
-    console.log(e);
+    log.error('Damn... Something went all wrong while downloading the schema files!');
+    log.error(e);
   });
 };
 
